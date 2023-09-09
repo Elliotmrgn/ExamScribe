2d542328149faa316cda13bf9d47138b01caeae9
Elliot Morgan <elliotmrgn@gmail.com>
2023-09-09 01:45:54 -0400

    table of contents and more

--- main.py
+++ main.py
@@ -10,50 +10,54 @@ import PySimpleGUI as sg
 
 
 def extract_chapter_outline(pdf_reader):
+    # Contains title and page number in table of contents (might not need)
+    table_of_contents = []
+    # Contains title and page number for chapters with questions
     chapter_outline = []
+    # Contains title and page number for chapters with answers
+    answer_outline = []
     for item in pdf_reader.outline:
         if isinstance(item, list):
             for subitem in item:
-                current = [subitem.title, pdf_reader.get_destination_page_number(subitem)]
-                chapter_outline.append(current)
+                table_of_contents.append([subitem.title, pdf_reader.get_destination_page_number(subitem)])
+                if subitem.title.startswith('Chapter '):
+                    chapter_outline.append([subitem.title, pdf_reader.get_destination_page_number(subitem)])
+                elif subitem.title.startswith('Answers to Chapter '):
+                    answer_outline.append([subitem.title, pdf_reader.get_destination_page_number(subitem)])
 
         else:
-            current = [item.title, pdf_reader.get_destination_page_number(item)]
-            chapter_outline.append(current)
-    return chapter_outline
+            table_of_contents.append([item.title, pdf_reader.get_destination_page_number(item)])
+            if item.title.startswith('Chapter '):
+                chapter_outline.append([item.title, pdf_reader.get_destination_page_number(item)])
+            elif item.title.startswith('Answers to Chapter '):
+                answer_outline.append([item.title, pdf_reader.get_destination_page_number(item)])
+    return table_of_contents, chapter_outline, answer_outline
 
+def extract_questions(pdf_reader):
+    pass
 # Function to open and process the selected PDF file
 def pdf_processing(file_path):
     try:
         pdf_reader = pypdf.PdfReader(file_path)
-        print(extract_chapter_outline(pdf_reader))
-
-        for item in pdf_reader.outline:
-            if isinstance(item, list):
-                for subitem in item:
-                    print(subitem.title)
-                    print(pdf_reader.get_destination_page_number(subitem))
-
-            else:
-                print(item.title)
-                print(pdf_reader.get_destination_page_number(item))
+        table_of_contents, chapter_outline, answer_outline = extract_chapter_outline(pdf_reader)
+        starting_page_num = chapter_outline[0][1]
 
         # extract image and save -- change filename to question number
         # for image in pdf_reader.pages[24].images:
         #     with open(image.name, "wb") as fp:
         #         fp.write(image.data)
 
-
         # Add more processing code here as needed
     except Exception as e:
         sg.popup_error(f"Error: {e}")
+
 
 # Function to load PDF titles and file paths from a text file
 def load_previous_pdfs():
     pass
 
-def nav_window():
 
+def nav_window():
     # Define the layout of the GUI
     layout = [
         [sg.Text("PDF Titles:")],
